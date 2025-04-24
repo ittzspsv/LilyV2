ed98a332bd2a229f2e0658b1989173e656cdba1d
SHREESPSV <70304530+ittzspsv@users.noreply.github.com>
2025-04-25 00:47:15 +0530

    Update sLilyEmbed.py

--- src/Misc/sLilyEmbed.py
+++ src/Misc/sLilyEmbed.py
@@ -138,3 +138,47 @@ def EmbedParser(config_str: str, ctx):
         embeds_to_display.append(embed)
 
     return embeds_to_display, buttons
+
+def ParseAdvancedEmbed(data: dict):
+    content = data.get("content") or None
+    embeds = []
+
+    for embed_data in data.get("embeds", []):
+        try:
+            color = discord.Color(embed_data.get("color", 0))
+        except (ValueError, TypeError):
+            color = discord.Color.default()
+
+        embed = discord.Embed(
+            title=embed_data.get("title"),
+            description=embed_data.get("description"),
+            url=embed_data.get("url"),
+            color=color
+        )
+
+        if "timestamp" in embed_data:
+            try:
+                embed.timestamp = datetime.fromtimestamp(int(embed_data["timestamp"]) / 1000)
+            except (ValueError, TypeError):
+                pass
+
+        if author := embed_data.get("author"):
+            embed.set_author(name=author.get("name"), url=author.get("url"), icon_url=author.get("icon_url"))
+        if thumbnail := embed_data.get("thumbnail"):
+            embed.set_thumbnail(url=thumbnail.get("url"))
+        if image := embed_data.get("image"):
+            embed.set_image(url=image.get("url"))
+        if footer := embed_data.get("footer"):
+            embed.set_footer(text=footer.get("text"), icon_url=footer.get("icon_url"))
+
+        for field in embed_data.get("fields", []):
+            embed.add_field(
+                name=field.get("name"),
+                value=field.get("value"),
+                inline=field.get("inline", False)
+            )
+
+        embeds.append(embed)
+
+    return content, embeds
+
